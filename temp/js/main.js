ae7bc73b406fde2c94a069ec1abda3ec4f17c277
Igor Tarasenko <tarasenko.igr@gmail.com>
2019-01-28 17:52:51 +0200

    add new blocks

--- temp/js/main.js
+++ temp/js/main.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
     $(function($){
         $("#phone").mask("+38(999)-99-99-999");
     });
+    $('.games-js').click(function () {
+        $('.games-js').removeClass('active');
+        $(this).addClass('active');
+    });
+    $(".more-js").click(function () {
+        $(this).parent().parent().find(".hidden-games-text").slideToggle();
+    });
+    $(".roadmap-js").click(function () {
+        $(".roadmap-js").removeClass('active');
+        $(this).addClass('active');
+    })
 });
