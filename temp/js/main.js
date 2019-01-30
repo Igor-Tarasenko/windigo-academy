ba782ba97fc422ada83c58e77a19ad95cf51b4bb
Igor Tarasenko <tarasenko.igr@gmail.com>
2019-01-30 17:40:11 +0200

    add scroll to block

--- temp/js/main.js
+++ temp/js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    $("a.topLink").click(function() {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+        return false;
+    });
     $(function($){
         $("#phone, #phone2").mask("+38(999)-99-99-999");
     });
